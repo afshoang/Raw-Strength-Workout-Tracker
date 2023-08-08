0b33e04ba0ea3b4238003224667995cf47b4ce85
afshoang <kaitokid19xx@gmail.com>
2023-08-08 10:14:15 +0700

    beta version

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useFonts } from "expo-font";
+import { Provider } from 'react-redux'
+import { PersistGate } from "redux-persist/integration/react"
+import { store, persistor } from './store';
+import { COLORS } from './constants';
+import { MainStackNavigator } from './navigation/StackNavigator';
+import { ActivityIndicator, View } from 'react-native'
 
-export default function App() {
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: COLORS.secondary,
+  }
+}
+// console.log("👌👌👌👌", store.getState());
+// export const purgeData = async () => {
+//   await persistor.purge();
+// };
+// purgeData()
+
+const App = () => {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!loaded) return null;
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={<View>
+        <ActivityIndicator size="large" color={COLORS.main} />
+      </View>} persistor={persistor}>
+        <NavigationContainer theme={theme}>
+          <MainStackNavigator />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
