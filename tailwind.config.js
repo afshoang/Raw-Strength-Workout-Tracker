803716beb8d75da51758aa65888458af2fcccd24
afshoang <kaitokid19xx@gmail.com>
2023-08-18 12:15:04 +0700

    implement tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    // content: ['./src/**/*.{html,js}'],
+    theme: {
+        colors: {
+            'blue': '#1fb6ff',
+            'purple': '#7e5bef',
+            'pink': '#ff49db',
+            'orange': '#ff7849',
+            'green': '#13ce66',
+            'yellow': '#ffc82c',
+            'gray-dark': '#273444',
+            'gray': '#8492a6',
+            'gray-light': '#d3dce6',
+            'primary': "rgb(42, 47, 55)",
+            'secondary': "rgb(28, 34, 39)",
+            'main': "#e0fe10",
+            // 'blue': "#0d6efd",
+            'white': "#FFF",
+            // 'gray': "#74858C",
+            'black': "#151e00"
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+            bold: ["InterBold"],
+            semiBold: ["InterSemiBold"],
+            medium: ["InterMedium"],
+            regular: ["InterRegular"],
+            light: ["InterLight"],
+        },
+        extend: {
+            spacing: {
+                '8xl': '96rem',
+                '9xl': '128rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        }
+    },
+}
